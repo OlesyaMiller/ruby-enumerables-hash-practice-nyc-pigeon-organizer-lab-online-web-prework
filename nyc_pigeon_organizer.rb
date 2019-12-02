c06068820d1df25764a7444fcf8b06e47954d7ff
OLESYA MILLER <olesyam.miller@gmail.com>
2019-12-02 23:05:42 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  new_hash = {}
+  
+  #color = (data[].map {|x| x.keys})
+  
+  color = data[:color]
+  gender = data[:gender]
+  lives = data[:lives]
+  
+  data.each do |key, value|
+    value.each do |key1, value1|
+      value1.each do |name|
+        new_hash[name] = {:color => [], :gender => [], :lives => []}
+      end
+    end
+     #binding.pry
+  end
+  
+  color.each do |key, value|
+    new_hash.each do |key1, value1|
+      #binding.pry 
+      if value.include?(key1)
+        new_hash[:color].push(key)
+        #if true get the color inside the color array 
+      end
+    end
+  end   
+  #binding.pry 
+  
 end
