1cdb22956f03394f42c7c3cd0d5dcd2012421c86
OLESYA MILLER <olesyam.miller@gmail.com>
2019-12-03 00:53:19 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,28 +1,24 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-
   new_hash = {}
-  
   color = data[:color]
   gender = data[:gender]
   lives = data[:lives]
   
-  data.each do |key, value|
+  data.each do |key, value| 
     value.each do |key1, value1|
       value1.each do |name|
+        #binding.pry
         new_hash[name] = {:color => [], :gender => [], :lives => []}
-        new_hash
       end
     end
   end
   
-  
   color.each do |key, value|
     new_hash.each do |key1, value1|
-      #binding.pry 
       if value.include?(key1)
-        new_hash[key1][:color] << key 
+        new_hash[key1][:color] << key.to_s
       end
     end
   end   
@@ -31,7 +27,7 @@ def nyc_pigeon_organizer(data)
     new_hash.each do |key1, value1|
       #binding.pry 
       if value.include?(key1)
-        new_hash[key1][:gender] << key 
+        new_hash[key1][:gender] << key.to_s  
       end
     end
   end   
@@ -44,7 +40,6 @@ def nyc_pigeon_organizer(data)
       end
     end
   end 
- 
-  #binding.pry 
+
   new_hash
 end
