a8c73058487f8d031c1b860eb4fb6ac3e4f8a473
OLESYA MILLER <olesyam.miller@gmail.com>
2019-12-03 00:21:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,15 +4,10 @@ def nyc_pigeon_organizer(data)
 
   new_hash = {}
   
-  color = data[:color]
-  gender = data[:gender]
-  lives = data[:lives]
-  
-  data.each do |key, value|
+  data.each do |key, value| 
     value.each do |key1, value1|
       value1.each do |name|
         new_hash[name] = {:color => [], :gender => [], :lives => []}
-        new_hash
       end
     end
   end
@@ -20,30 +15,30 @@ def nyc_pigeon_organizer(data)
   
   color.each do |key, value|
     new_hash.each do |key1, value1|
-      #binding.pry 
+      binding.pry 
       if value.include?(key1)
         new_hash[key1][:color] << key 
       end
     end
   end   
   
-  gender.each do |key, value|
-    new_hash.each do |key1, value1|
-      #binding.pry 
-      if value.include?(key1)
-        new_hash[key1][:gender] << key 
-      end
-    end
-  end   
+  # gender.each do |key, value|
+  #   new_hash.each do |key1, value1|
+  #     #binding.pry 
+  #     if value.include?(key1)
+  #       new_hash[key1][:gender] << key 
+  #     end
+  #   end
+  # end   
   
-  lives.each do |key, value|
-    new_hash.each do |key1, value1|
-      #binding.pry 
-      if value.include?(key1)
-        new_hash[key1][:lives] << key 
-      end
-    end
-  end 
+  # lives.each do |key, value|
+  #   new_hash.each do |key1, value1|
+  #     #binding.pry 
+  #     if value.include?(key1)
+  #       new_hash[key1][:lives] << key 
+  #     end
+  #   end
+  # end 
  
   #binding.pry 
   new_hash
