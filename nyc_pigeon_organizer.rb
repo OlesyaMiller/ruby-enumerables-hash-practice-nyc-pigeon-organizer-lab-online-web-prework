9830dddff41e97f2389ab098384ca0890074d15c
OLESYA MILLER <olesyam.miller@gmail.com>
2019-12-03 00:27:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-binding.pry
   new_hash = {}
   
   data.each do |key, value| 
     value.each do |key1, value1|
       value1.each do |name|
+        binding.pry
         new_hash[name] = {:color => [], :gender => [], :lives => []}
       end
     end
